6d04782ce609edfd0724a1c5e8d1a85a95f50ad6
seveibar <seveibar@gmail.com>
2016-09-26 09:28:18 -0400

    sql boilerplate

--- src/db/index.js
+++ src/db/index.js
@@ -2,4 +2,23 @@
  * Internal module for database queries
  **/
 
- 
+
+function connect() {
+  return new Promise((accept, reject) => {
+    reject();
+  });
+}
+
+function addSurveyResponse(){
+  return new Promise((accept, reject) => {
+    reject();
+  });
+}
+
+function getSurveyResponses(){
+  return new Promise((accept, reject) => {
+    reject();
+  });
+}
+
+ export default { connect, addSurveyResponse, getSurveyResponses };
