bda0b1a5ce17dcccf58361c2a05e67992ec5e9f7
seveibar <seveibar@gmail.com>
2016-10-20 13:35:58 -0400

    added user to initialization seed

--- src/config/seed.js
+++ src/config/seed.js
@@ -1,5 +1,7 @@
 import surveySeed from '../api/survey/survey.seed';
+import userSeed from '../api/survey/user.seed';
 
 export default async function(){
   await surveySeed();
+  await userSeed();
 }
