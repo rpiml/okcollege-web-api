903f65a64117a76a3273d7b0384f9d296c53e5a9
Andrew Kaiser <kaisea@rpi.edu>
2016-11-22 02:01:35 -0500

    add user seed

--- src/api/user/user.seed.js
+++ src/api/user/user.seed.js
@@ -3,6 +3,12 @@ import User from './user.model';
 
 export default async function(){
   await User.sync({force: true});
+    await User.create({
+        firstName: "Rick",
+        lastName: "Sanchez",
+        email: "ricky@morty.com",
+        password: "squanch"
+    })
   // TODO create seed surveys
 }
 
