0a0d64218790edd26ab11296ee226f42ae17acb6
jowlee <joeylee0925@gmail.com>
2016-11-26 18:33:57 -0500

    api auth

--- src/config/express.js
+++ src/config/express.js
@@ -6,10 +6,11 @@
 
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser'
 
 export default function(app) {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(morgan('dev'));
-
+  app.use(cookieParser())
 }
